fe1068955c53910dd32195883ca8cf8ccfed6979
evasousa23 <sousa.eva99@gmail.com>
2023-12-19 21:12:25 +0000

    added the api integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "saudade",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. ";
+  let prompt = `User instructions:Generate a Portuguese about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+ 
 
 }
 
