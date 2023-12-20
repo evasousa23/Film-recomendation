2ec1f4fda3b487bf97afa7a4f29558cd060e7580
evasousa23 <sousa.eva99@gmail.com>
2023-12-20 16:42:11 +0000

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ function generatePoem(event) {
   let prompt = `User instructions:Generate a Portuguese about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+
+  let poemElement=document.querySelector("#poem")
+poemElement.classList.remove("hidden")
+poemElement.innerHTML=`Generating a poem about ${instructionsInput.value}`
+
+
+
   axios.get(apiUrl).then(displayPoem);
 
  
