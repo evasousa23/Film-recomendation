029a8fa1eb6502c267b6ce6d5fc544cba6eae84c
evasousa23 <sousa.eva99@gmail.com>
2023-12-20 18:40:56 +0000

    improve the user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-function displayPoem(response) {
-  new Typewriter("#poem", {
+function displayFilm(response) {
+  new Typewriter("#film", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
@@ -8,28 +8,28 @@ function displayPoem(response) {
 
 }
 
-function generatePoem(event) {
+function generateFilm(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "a3af3t89ob96e0f9a620e92909b7a4bf";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. ";
-  let prompt = `User instructions:Generate a Portuguese about ${instructionsInput.value}`;
+    "You are a AI incredible expert cinematic and love recomend films. You mission is to generate  a recomendation in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Include the title of the film in <h1> ";
+  let prompt = `User instructions:Generate a film recomendations about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 
-  let poemElement=document.querySelector("#poem")
-poemElement.classList.remove("hidden")
-poemElement.innerHTML=`Generating a poem about ${instructionsInput.value}`
+  let filmElement=document.querySelector("#film")
+filmElement.classList.remove("hidden")
+filmElement.innerHTML = ` <div class="generating">⏱️Generating a film recomendations about ${instructionsInput.value}</div>`;
 
 
 
-  axios.get(apiUrl).then(displayPoem);
+  axios.get(apiUrl).then(displayFilm);
 
  
 
 }
 
-let poemFormElement = document.querySelector("#poem-generator");
-poemFormElement.addEventListener("submit", generatePoem);
+let filmFormElement = document.querySelector("#film-generator");
+filmFormElement.addEventListener("submit", generateFilm);
